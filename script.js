ac743fabb2d4b0cdeefe486336520f2b0bef31a3
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-01-21 18:15:54 +0000

    added JS for arrow

--- script.js
+++ script.js
@@ -59,3 +59,23 @@ document.body.addEventListener("mouseleave", () =>{
   mouseDot.style.opacity = "0";
 });
 
+//main button
+const mainBtn = document.querySelector(".main-btn");
+
+let ripple;
+
+mainBtn.addEventListener("mouseenter", (e) => {
+  const left = e.clientX - e.target.getBoundingClientRect().left;
+  const top = e.clientY - e.target.getBoundingClientRect().top;
+
+  ripple = document.createElement("div");
+  ripple.classList.add("ripple");
+  ripple.style.left = `${left}px`;
+  ripple.style.top = `${top}px`;
+  mainBtn.prepend(ripple);
+});
+
+mainBtn.addEventListener("mouseleave", () => {
+  mainBtn.removeChild(ripple);
+});
+//end of main button
