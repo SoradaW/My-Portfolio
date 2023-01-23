af8eaacf70260166d1a5f25b7ad7f0db3b94b3ea
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-01-23 19:18:21 +0000

    Added form HTML and CSS

--- script.js
+++ script.js
@@ -187,6 +187,16 @@ document.querySelectorAll(".service-btn").forEach(service => {
 
     const serviceText = service.nextElementSibling;
     serviceText.classList.toggle("change");
+
+    const rightPosition = serviceText.classList.contains("change")? `calc(100% - ${getComputedStyle(service.firstElementChild).width})`: 0;
+
+    service.firstElementChild.style.right = rightPosition;
   });
 });
 //end of section 4
+
+//section 5
+
+
+//end of section 5
+
