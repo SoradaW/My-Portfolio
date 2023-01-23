3a1aecf730e2a2f063773ab1ba5d9fead352b520
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-01-23 20:44:27 +0000

    Updated Icons

--- script.js
+++ script.js
@@ -219,5 +219,30 @@ formInputs.forEach((input) => {
 });
 //end of form
 
+//slideshow
+const slideshow = document.querySelector(".slideshow");
+
+setInterval(() => {
+  const firstIcon = slideshow.firstElementChild;
+
+  firstIcon.classList.add("faded-out");
+
+  const thirdIcon = slideshow.children[3];
+
+  thirdIcon.classList.add("light");
+
+  thirdIcon.previousElementSibling.classList.remove("light");
+
+  setTimeout(() => {
+    slideshow.removeChild(firstIcon);
+
+    slideshow.appendChild(firstIcon);
+
+    setTimeout(() => {
+      firstIcon.classList.remove("faded-out");
+    }, 500);
+  }, 500);
+}, 3000);
+//end of slideshow
 //end of section 5
 
