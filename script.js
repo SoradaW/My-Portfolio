2d739a870dfe0e7a45ac01e2ad582008b7d5975b
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-01-23 19:27:17 +0000

    Added JS code for form

--- script.js
+++ script.js
@@ -196,7 +196,28 @@ document.querySelectorAll(".service-btn").forEach(service => {
 //end of section 4
 
 //section 5
+//form
+const formHeading = document.querySelector(".form-heading");
+const formInputs = document.querySelectorAll(".contact-form-input");
 
+formInputs.forEach((input) => {
+  input.addEventListener("focus", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => { 
+    formHeading.textContent = `Your ${input.placeholder}`;
+    formHeading.style.opacity = "1";
+    },300);
+  });
+
+  input.addEventListener("blur", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => { 
+    formHeading.textContent = "Let's Talk";
+    formHeading.style.opacity = "1";
+    },300);
+  });
+});
+//end of form
 
 //end of section 5
 
