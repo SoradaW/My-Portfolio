872df4193599b9eb812abfff33bd9aa7a636ba62
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-02-12 19:48:34 +0000

    Added validation function in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -418,7 +418,58 @@ setInterval(() => {
 //end of slideshow
 
 //form validation
+const form = document.querySelector(".contact-form");
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+const messages = document.querySelectorAll(".message");
 
+const error = (input, message) => {
+  input.nextElementSibling.classList.add("error");
+  input.nextElementSibling.textContent = message;
+};
+
+const success = (input) => {
+  input.nextElementSibling.classList.remove(".error");
+};
+
+const checkRequiredFields = (inputArr) => {
+  inputArr.forEach(input => {
+    if (input.value.trim() === "") {
+      error(input, `${input.id} is required`);
+    };
+  });
+};
+
+const checkLength = (input, min) => {
+  if (input.value.trim().length < min) {
+    error(input, `${input.id} must be at least ${min} characters`);
+  } else {
+    success(input);
+  };
+};
+
+const checkEmail = (input) => {
+  const regEx =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  if (regEx.test(input.value.trim())) {
+    success(input);
+  } else {
+    error(input, "Email is not valid");
+  };
+};
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  checkLength(username, 3);
+  checkLength(subject, 3);
+  checkLength(message, 10);
+  checkEmail(email);
+  checkRequiredFields([username, email, subject, message]);
+});
 //end of form validation
 
 //end of section 5
